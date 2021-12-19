e4610bacf608f349987cdbb69765d56ade0222ab
csh1314 <cshsb@qq.com>
2021-12-19 13:27:42 +0800

    feat: binary search

---
+++ algorithm-template/BinarySearch.js
@@ -0,0 +1,26 @@
+// 在nums中查找第一个 > target的index
+const upper_bound = (nums, target) => {
+  let l = 0, r = nums.length-1
+  while(l < r){
+    let m = (l+r) >> 1
+    if(nums[m] > target){
+      r = m
+    }else{
+      l = m+1
+    }
+  }
+  return l
+}
+
+// 在nums中查找第一个 >= target的index
+const lower_bound = (nums, target) => {
+  while(l < r){
+    let m = (l+r) >> 1
+    if(nums[m] >= target){
+      r = m
+    }else{
+      l = m+1
+    }
+  }
+  return l
+}
