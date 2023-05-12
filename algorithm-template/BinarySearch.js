24bdeb0fe8fbd78293d37819750c9e3abfa40e84
csh1314 <cshsb@qq.com>
2023-05-12 15:49:37 +0800

    fix: fix

--- algorithm-template/BinarySearch.js
+++ algorithm-template/BinarySearch.js
@@ -14,6 +14,7 @@ const upper_bound = (nums, target) => {
 
 // 在nums中查找第一个 >= target的index
 const lower_bound = (nums, target) => {
+  let l = 0, r = nums.length-1
   while(l < r){
     let m = Math.floor((l+r)/2)
     if(nums[m] >= target){
